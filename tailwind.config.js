21377cba9a540d9773be5cca4c61e0aa8b5c4bbe
Aakash Verma <dev.aakashv@gmail.com>
2024-04-12 20:23:00 +0530

    `add`: the colors for the application

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        stone100: 'hsl(30, 54%, 90%)',
+        stone150: 'hsl(30, 18%, 87%)',
+        stone600: 'hsl(30, 10%, 34%)',
+        stone900: 'hsl(24, 5%, 18%)',
+        brown800: 'hsl(14, 45%, 36%)',
+        rose800: 'hsl(332, 51%, 32%)',
+        rose50: 'hsl(330, 100%, 98%)',
+      },
+      colors: {
+        stone100: 'hsl(30, 54%, 90%)',
+        stone150: 'hsl(30, 18%, 87%)',
+        stone600: 'hsl(30, 10%, 34%)',
+        stone900: 'hsl(24, 5%, 18%)',
+        brown800: 'hsl(14, 45%, 36%)',
+        rose800: 'hsl(332, 51%, 32%)',
+        rose50: 'hsl(330, 100%, 98%)',
+      }
+    },
   },
   plugins: [],
 }
