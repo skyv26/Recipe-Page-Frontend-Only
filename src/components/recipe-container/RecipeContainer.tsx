66f3d66fabc41af69ecc81066dfd0a2f24da3718
Aakash Verma <dev.aakashv@gmail.com>
2024-04-13 21:14:07 +0530

    `check`: the context working and make sure that data fetched successful and render properly

--- src/components/recipe-container/RecipeContainer.tsx
+++ src/components/recipe-container/RecipeContainer.tsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { RecipeContext } from "../../context/RecipeContext";
+import RecipeImage from "../recipe-image/RecipeImage";
+
 const RecipeContainer = () => {
-  return (
-    <div>recipeContainer</div>
-  )
+  const { recipes, loading, error } = useContext(RecipeContext);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if(!recipes) return <></>;
+  
+  return <RecipeImage recipe_img={recipes[0]?.img} className=""  />
 }
 
 export default RecipeContainer
