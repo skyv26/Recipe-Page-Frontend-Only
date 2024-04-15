06a4a5821d5daab4b21d9e11c343e3f8f1fcbdd9
Aakash Verma <dev.aakashv@gmail.com>
2024-04-15 15:58:53 +0530

    `update`: component class by adding style as per the figma

--- src/components/ingredients/Ingredients.tsx
+++ src/components/ingredients/Ingredients.tsx
@@ -5,10 +5,10 @@ const { Title } = Typography;
 const Ingredients = ({ingredients}: {ingredients: string[]}) => {
   return (
     <>
-      <Title className='font-young_serif' level={2}>Ingredients</Title>
-      <ul className="list-disc pl-4 font-outfit">
+      <Title className='font-young_serif !text-brown800' level={2}>Ingredients</Title>
+      <ul className="list-disc pl-4 font-outfit text-stone600 marker:text-brown800">
         {
-          ingredients.map((each, index) => <li key={index}>{each}</li>)
+          ingredients.map((each, index) => <li className="pl-4" key={index}>{each}</li>)
         }
       </ul>
     </>
