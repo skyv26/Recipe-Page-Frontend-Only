6ed95554961f20c7a4d8648417b746438b78433a
Aakash Verma <dev.aakashv@gmail.com>
2024-04-16 16:29:18 +0530

    `update`: the component classes by adding the space according to design

--- src/components/recipe-detail/RecipeDetail.tsx
+++ src/components/recipe-detail/RecipeDetail.tsx
@@ -10,14 +10,16 @@ const RecipeDetail: React.FC<RecipeDetailProps> = ({
   preparationTime,
 }) => {
   return (
-    <div className="flex flex-col">
-      <Title level={1} className="font-young_serif !text-stone900">
-        {recipeName}
-      </Title>
-      <p className="font-outfit text-stone600">{recipeDescription}</p>
-      <div className="bg-rose50">
+    <div className="flex flex-col mt-3 gap-4 min-[375px]:mt-10 min-[375px]:gap-8">
+      <div className="flex flex-col gap-2 min-[375px]:gap-6">
+        <Title level={1} className="font-young_serif !text-stone900 !text-[30px]">
+          {recipeName}
+        </Title>
+        <p className="font-outfit text-stone600 text-base">{recipeDescription}</p>
+      </div>
+      <div className="bg-rose50 p-6 flex flex-col gap-4">
         <p className="font-outfit font-bold text-rose800">Preparation time</p>
-        <ul className="list-disc pl-4 font-outfit marker:text-rose800">
+        <ul className="flex flex-col gap-2 list-disc pl-4 font-outfit marker:text-rose800">
           {Object.entries(preparationTime).map(([key, value]) => (
             <li className="text-stone600 pl-4" key={key}>
               <b>{key.charAt(0).toUpperCase() + key.slice(1)}</b>: {value}
