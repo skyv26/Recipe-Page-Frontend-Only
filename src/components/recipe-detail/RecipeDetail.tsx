cd01badd3d0e1e6b9d86e323f5a8070d950f5d79
Aakash Verma <dev.aakashv@gmail.com>
2024-04-16 15:59:37 +0530

    `update`: component class by adding style as per the figma

--- src/components/recipe-detail/RecipeDetail.tsx
+++ src/components/recipe-detail/RecipeDetail.tsx
@@ -11,15 +11,15 @@ const RecipeDetail: React.FC<RecipeDetailProps> = ({
 }) => {
   return (
     <div className="flex flex-col">
-      <Title level={1} className="font-young_serif">
+      <Title level={1} className="font-young_serif !text-stone900">
         {recipeName}
       </Title>
-      <p className="font-outfit">{recipeDescription}</p>
-      <div>
-        <p className="font-outfit">Preparation time</p>
-        <ul className="list-disc pl-4 font-outfit">
+      <p className="font-outfit text-stone600">{recipeDescription}</p>
+      <div className="bg-rose50">
+        <p className="font-outfit font-bold text-rose800">Preparation time</p>
+        <ul className="list-disc pl-4 font-outfit marker:text-rose800">
           {Object.entries(preparationTime).map(([key, value]) => (
-            <li key={key}>
+            <li className="text-stone600 pl-4" key={key}>
               <b>{key.charAt(0).toUpperCase() + key.slice(1)}</b>: {value}
             </li>
           ))}
