d86587f58ec9d22cea95c97aec1dc7cde314488d
Aakash Verma <dev.aakashv@gmail.com>
2024-04-17 17:25:40 +0530

    `update`: the component classes by adding the space according to design

--- src/components/ingredients/Ingredients.tsx
+++ src/components/ingredients/Ingredients.tsx
@@ -5,7 +5,7 @@ const { Title } = Typography;
 const Ingredients = ({ingredients}: {ingredients: string[]}) => {
   return (
     <div className='mt-5 min-[375px]:mt-8 border-b pb-5 min-[375px]:pb-8'>
-      <Title className='font-young_serif !text-brown800' level={2}>Ingredients</Title>
+      <Title className='font-young_serif !text-brown800 !text-[28px]' level={2}>Ingredients</Title>
       <ul className="list-disc pl-4 font-outfit text-stone600 marker:text-brown800 flex flex-col gap-2">
         {
           ingredients.map((each, index) => <li className="pl-4" key={index}>{each}</li>)
