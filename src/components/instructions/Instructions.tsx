ef38c5b14097499f8dad7c141f3ce83e763e895d
Aakash Verma <dev.aakashv@gmail.com>
2024-04-17 17:25:52 +0530

    `update`: the component classes by adding the space according to design

--- src/components/instructions/Instructions.tsx
+++ src/components/instructions/Instructions.tsx
@@ -7,7 +7,7 @@ const Instructions = ({ instructions }: {
 }) => {
   return (
     <div className="mt-5 min-[375px]:mt-8 border-b pb-5 min-[375px]:pb-8">
-    <Title className='font-young_serif !text-brown800' level={3}>Instructions</Title>
+    <Title className='font-young_serif !text-brown800 !text-[28px]' level={3}>Instructions</Title>
     <ol className="list-decimal flex flex-col gap-2 pl-4 font-outfit marker:text-brown800 marker:!font-bold">
       {
         instructions.map((each, index) => {
