0d3b4099908f9f0cb79b0967debe6197fde068c0
Aakash Verma <dev.aakashv@gmail.com>
2024-04-18 19:08:02 +0530

    `update`: the component classes by adding the space according to design

--- src/components/instructions/Instructions.tsx
+++ src/components/instructions/Instructions.tsx
@@ -6,13 +6,13 @@ const Instructions = ({ instructions }: {
   instructions: string[]
 }) => {
   return (
-    <div className="mt-5 min-[375px]:mt-8 border-b pb-5 min-[375px]:pb-8">
-    <Title className='font-young_serif !text-brown800 !text-[28px]' level={3}>Instructions</Title>
-    <ol className="list-decimal flex flex-col gap-2 pl-4 font-outfit marker:text-brown800 marker:!font-bold">
+    <div className="mt-5 flex flex-col min-[375px]:mt-7 border-b pb-5 min-[375px]:pb-8 min-[375px]:gap-1">
+    <Title className='font-young_serif !text-brown800 !font-normal !text-[28px]' level={3}>Instructions</Title>
+    <ol className="list-decimal flex flex-col gap-2 pl-4 font-outfit marker:text-brown800 marker:!font-bold min-[375px]:pl-[23px]">
       {
         instructions.map((each, index) => {
           const [heading, content] = each.split(":");
-          return <li className="text-stone600 pl-4" key={index}>
+          return <li className="text-stone600 pl-4 min-[375px]:!text-base tracking-[-0.01em] leading-[1.05]" key={index}>
             <b className="font-bold">{heading}</b>: {content}
           </li>
         })
