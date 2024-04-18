6e55cb34e8b1600bc96adf9bb0d209bf0926c847
Aakash Verma <dev.aakashv@gmail.com>
2024-04-18 19:08:14 +0530

    `update`: the component classes by adding the space according to design

--- src/components/recipe-detail/RecipeDetail.tsx
+++ src/components/recipe-detail/RecipeDetail.tsx
@@ -10,16 +10,16 @@ const RecipeDetail: React.FC<RecipeDetailProps> = ({
   preparationTime,
 }) => {
   return (
-    <div className="flex flex-col mt-3 gap-4 min-[375px]:mt-10 min-[375px]:gap-8">
-      <div className="flex flex-col gap-2 min-[375px]:gap-6">
-        <Title level={1} className="font-young_serif !text-stone900 !text-[30px]">
+    <div className="flex flex-col mt-3 gap-4 min-[375px]:gap-8">
+      <div className="flex flex-col gap-1">
+        <Title level={1} className="font-young_serif !text-stone900 !text-[30px] !font-normal min-[375px]:!text-[2.231rem] !leading-[1.05]">
           {recipeName}
         </Title>
-        <p className="font-outfit text-stone600 text-base">{recipeDescription}</p>
+        <p className="font-outfit text-stone600 text-base min-[375px]:tracking-normal">{recipeDescription}</p>
       </div>
-      <div className="bg-rose50 p-6 flex flex-col gap-4">
-        <p className="font-outfit font-bold text-rose800">Preparation time</p>
-        <ul className="flex flex-col gap-2 list-disc pl-4 font-outfit marker:text-rose800">
+      <div className="bg-rose50 p-6 min-[375px]:py-5 flex flex-col gap-4 min-[375px]:gap-2">
+        <p className="font-outfit font-bold text-rose800 min-[375px]:text-[20px]">Preparation time</p>
+        <ul className="flex flex-col gap-2 list-disc pl-4 font-outfit marker:text-rose800 min-[375px]:marker:text-[12px] min-[375px]:pl-6 min-[375px]:gap-2 min-[375px]:text-base">
           {Object.entries(preparationTime).map(([key, value]) => (
             <li className="text-stone600 pl-4" key={key}>
               <b>{key.charAt(0).toUpperCase() + key.slice(1)}</b>: {value}
