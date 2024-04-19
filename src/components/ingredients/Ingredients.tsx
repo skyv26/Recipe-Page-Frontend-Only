1d9c501713cfb5291942fd38df80c6c12ea461ed
Aakash Verma <dev.aakashv@gmail.com>
2024-04-20 01:23:12 +0530

    `fix`: the border color as per design

--- src/components/ingredients/Ingredients.tsx
+++ src/components/ingredients/Ingredients.tsx
@@ -4,7 +4,7 @@ const { Title } = Typography;
 
 const Ingredients = ({ingredients}: {ingredients: string[]}) => {
   return (
-    <div className='mt-5 flex flex-col min-[375px]:mt-8 border-b pb-5 min-[375px]:pb-8 min-[375px]:gap-2'>
+    <div className='mt-5 flex flex-col min-[375px]:mt-8 border-b border-[#E3DDD7] pb-5 min-[375px]:pb-8 min-[375px]:gap-2'>
       <Title className='font-young_serif !text-brown800 !font-normal !text-[28px]' level={2}>Ingredients</Title>
       <ul className="list-disc  pl-4 font-outfit text-stone600 marker:text-brown800 flex flex-col gap-2 min-[375px]:gap-2 min-[375px]:marker:text-[12px] min-[375px]:pl-[23px]">
         {
