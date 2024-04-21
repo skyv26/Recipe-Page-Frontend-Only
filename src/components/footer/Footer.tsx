6de019883f9a4dc4b58594f802e32a7d5d39a45d
Aakash Verma <dev.aakashv@gmail.com>
2024-04-22 01:41:26 +0530

    `add`: the footer component to the application

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,9 @@
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer className="attribution text-stone900 !font-bold !text-sm">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="!text-rose800">Frontend Mentor</a>. 
+    Coded with 💜 by <a href="https://linkedin.com/in/skyv2022" target="_blank" rel="noopener noreferrer" className="!text-rose800">Aakash Verma</a>.
+  </footer>
   )
 }
 
