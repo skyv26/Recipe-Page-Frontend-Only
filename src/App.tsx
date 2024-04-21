af6df0069ab35034d613d7e1041f481c5bfd03cd
Aakash Verma <dev.aakashv@gmail.com>
2024-04-22 01:42:59 +0530

    `add`: the footer component to the `App`

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 // import { useSwipeable } from "react-swipeable";
 import RecipeContainer from "./components/recipe-container/RecipeContainer";
 import SwipeComponent from "./utils/SwipeableComponent";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   
   return <SwipeComponent currentIndex={currentIndex} setCurrentIndex={setCurrentIndex}>
       <RecipeContainer currentIndex={currentIndex} />
+      <Footer />
     </SwipeComponent>
 } 
   
