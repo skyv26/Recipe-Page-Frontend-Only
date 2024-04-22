a8e67c25ffc1aa889347c85725966ab4154b2868
Aakash Verma <dev.aakashv@gmail.com>
2024-04-23 01:54:01 +0530

    `refactor`: the text size as per the design

--- src/components/recipe-container/RecipeContainer.tsx
+++ src/components/recipe-container/RecipeContainer.tsx
@@ -13,8 +13,8 @@ const RecipeContainer: React.FC<RecipeContainerProps> = ({currentIndex}) => {
   if (error) return <div>Error: {error}</div>;
   if(!recipes) return <></>;
   return <div className="bg-white min-[580px]:rounded-[24px] min-[580px]:p-[40px] min-[580px]:w-10/12 min-[580px]:mx-auto min-[580px]:my-32">
-    <RecipeImage recipeImg={recipes[currentIndex]?.img} className="!w-full !min-h-[171px] min-[580px]:!h-auto min-[580px]:rounded-[12px]"  />
-    <div className="p-5 min-[375px]:px-8 min-[375px]:py-7 bg-white min-[580px]:px-0">
+    <RecipeImage recipeImg={recipes[currentIndex]?.img} className="!w-full !min-h-[171px] min-[580px]:!h-auto min-[580px]:rounded-[12px] md:!min-h-[300px]"  />
+    <div className="p-5 min-[375px]:px-8 min-[375px]:py-7 bg-white min-[580px]:px-0 md:!p-0 md:mt-10">
       <RecipeDetail recipeName={recipes[currentIndex]?.recipeName} recipeDescription={recipes[currentIndex]?.description} preparationTime={recipes[currentIndex]?.preparationTime} />
       <Ingredients ingredients={recipes[currentIndex]?.ingredients} />
       <Instructions instructions={recipes[currentIndex]?.instructions} />
