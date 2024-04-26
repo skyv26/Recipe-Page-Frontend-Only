53155dea2f26827f7133f1df4def7b1d721bf213
Aakash Verma <dev.aakashv@gmail.com>
2024-04-26 14:35:12 +0530

    `add`: the `RecipeDropDown` component`

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import { useSwipeable } from "react-swipeable";
+import Footer from "./components/footer/Footer";
 import RecipeContainer from "./components/recipe-container/RecipeContainer";
 import SwipeComponent from "./utils/SwipeableComponent";
-import Footer from "./components/footer/Footer";
+import RecipeDropDown from "./components/RecipeDropDown/RecipeDropDown";
 
 const App = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowDropDown(true);
+    }, 5000); // 10 seconds
+
+    // Cleanup the timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return <SwipeComponent currentIndex={currentIndex} setCurrentIndex={setCurrentIndex}>
+      {
+        showDropDown &&
+      <RecipeDropDown currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} />
+      }
       <RecipeContainer currentIndex={currentIndex} />
       <Footer />
     </SwipeComponent>
